a1bcc696305eea6d717b7cee226acc2e1bd81d2d
Roman Shreiter <Ukkitop@gmail.com>
2021-11-18 00:07:38 +0100

    init commit

---
+++ Word traning/Word_traningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_traningApp.swift
+//  Word traning
+//
+//  Created by Roman on 17.11.21.
+//
+
+import SwiftUI
+
+@main
+struct Word_traningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
